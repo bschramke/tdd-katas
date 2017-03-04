d9e95847978e20a7ff1bd5c93880ea1c6f768c05
Björn Schramke <bjoern.schramke@gmail.com>
2017-03-04 18:06:51 +0100

    [roman-numerals] refactoring

--- roman-numerals/src/main/java/com/github/bschramke/tddkatas/RomanNumeralsConverter.java
+++ roman-numerals/src/main/java/com/github/bschramke/tddkatas/RomanNumeralsConverter.java
@@ -1,13 +1,16 @@
 package com.github.bschramke.tddkatas;
 
 public class RomanNumeralsConverter {
-    public String fromArabic(int arabic) {
+    public String fromArabic(final int arabic) {
         if(arabic < 0) throw new IllegalArgumentException();
         if(arabic == 0) return "N";
-        if(arabic == 1) return "I";
-        if(arabic == 2) return "II";
-        if(arabic == 3) return "III";
 
-        return null;
+        final StringBuilder result = new StringBuilder();
+        int remaining = arabic;
+        while(0 < remaining--){
+            result.append('I');
+        }
+
+        return result.toString();
     }
 }
