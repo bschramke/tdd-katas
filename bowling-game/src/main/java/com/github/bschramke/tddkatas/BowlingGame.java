a94c19f23ed1afb095f40bb82540c71aba5420f0
Björn Schramke <bjoern.schramke@epost-dev.de>
2017-03-16 15:49:29 +0100

    [bowling-kata] initial setup

---
+++ bowling-game/src/main/java/com/github/bschramke/tddkatas/BowlingGame.java
@@ -0,0 +1,4 @@
+package com.github.bschramke.tddkatas;
+
+public class BowlingGame {
+}
---
+++ bowling-game/src/test/groovy/com/github/bschramke/tddkatas/BowlingGameSpec.groovy
@@ -0,0 +1,7 @@
+package com.github.bschramke.tddkatas
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class BowlingGameSpec extends Specification {
+}
